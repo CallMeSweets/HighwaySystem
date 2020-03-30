e599a0aa77ba2d2ac7d54530e5cb151a3b691d16
Wiktor Łazarski <wjlazarski@gmail.com>
2020-03-30 18:26:53 +0200

    Reshaped IndexMinPQ class

--- src/main/java/algorithm/IndexMinPQ.java
+++ src/main/java/algorithm/IndexMinPQ.java
@@ -1,9 +1,8 @@
 package algorithm;
 
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
+public class IndexMinPQ<Key extends Comparable<Key>> {
     private int maxN;        // maximum number of elements on PQ
     private int n;           // number of elements on PQ
     private int[] pq;        // binary heap using 1-based indexing
@@ -44,16 +43,6 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
         swim(n);
     }
 
-    public int minIndex() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
-        return pq[1];
-    }
-
-    public Key minKey() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
-        return keys[pq[1]];
-    }
-
     public int delMin() {
         if (n == 0) throw new NoSuchElementException("Priority queue underflow");
         int min = pq[1];
@@ -66,12 +55,6 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
         return min;
     }
 
-    public Key keyOf(int i) {
-        validateIndex(i);
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        else return keys[i];
-    }
-
     public void changeKey(int i, Key key) {
         validateIndex(i);
         if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
@@ -82,39 +65,6 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 
     public void change(int i, Key key) {
         changeKey(i, key);
-    }
-
-    public void decreaseKey(int i, Key key) {
-        validateIndex(i);
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        if (keys[i].compareTo(key) == 0)
-            throw new IllegalArgumentException("Calling decreaseKey() with a key equal to the key in the priority queue");
-        if (keys[i].compareTo(key) < 0)
-            throw new IllegalArgumentException("Calling decreaseKey() with a key strictly greater than the key in the priority queue");
-        keys[i] = key;
-        swim(qp[i]);
-    }
-
-    public void increaseKey(int i, Key key) {
-        validateIndex(i);
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        if (keys[i].compareTo(key) == 0)
-            throw new IllegalArgumentException("Calling increaseKey() with a key equal to the key in the priority queue");
-        if (keys[i].compareTo(key) > 0)
-            throw new IllegalArgumentException("Calling increaseKey() with a key strictly less than the key in the priority queue");
-        keys[i] = key;
-        sink(qp[i]);
-    }
-
-    public void delete(int i) {
-        validateIndex(i);
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        int index = qp[i];
-        exch(index, n--);
-        swim(index);
-        sink(index);
-        keys[i] = null;
-        qp[i] = -1;
     }
 
     // throw an IllegalArgumentException if i is an invalid index
@@ -149,29 +99,6 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
             if (!greater(k, j)) break;
             exch(k, j);
             k = j;
-        }
-    }
-
-    public Iterator<Integer> iterator() { return new HeapIterator(); }
-
-    private class HeapIterator implements Iterator<Integer> {
-        // create a new pq
-        private IndexMinPQ<Key> copy;
-
-        // add all elements to copy of heap
-        // takes linear time since already in heap order so no keys move
-        public HeapIterator() {
-            copy = new IndexMinPQ<Key>(pq.length - 1);
-            for (int i = 1; i <= n; i++)
-                copy.insert(pq[i], keys[pq[i]]);
-        }
-
-        public boolean hasNext()  { return !copy.isEmpty();                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Integer next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            return copy.delMin();
         }
     }
  
